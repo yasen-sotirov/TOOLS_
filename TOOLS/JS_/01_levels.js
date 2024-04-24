4afd619296be378b5ca059e43fd10453a27aba63
Yasen Sotirov <ia.sotirov@gmail.com>
2024-04-24 17:23:24 +0300

    new tools

---
+++ TOOLS/JS_/01_levels.js
@@ -0,0 +1,48 @@
+/* JS LEVELS
+https://www.youtube.com/watch?v=-3bVuFcteUo&ab_channel=TechWithTim
+
+-= BEGINNER=-
+
+Variables: 
+    var, let, const
+
+Data Types: 
+    Strings, Numbers, Booleans, null, undefined.
+
+Operators: 
+    Arithmetic, Assignment, Comparison, Logical, 
+    
+Conditionals
+Control Flow: if/else, switch, ternary operator.
+
+Basic Data Structures: 
+    Arrays, Objects, Mutability.
+
+Loops: 
+    for, while, do...while, iterating over arrays with forEach.
+
+Functions: 
+    Declaration, Invocation, Return, Parameters vs Arguments.
+
+Common Methods
+    Sets, Maps
+
+Advanced Functions: 
+    Arrow functions, default parameters, rest parameters.
+
+Scope
+
+DOM Manipulation: 
+    Selecting and changing webpage elements, handling user input.
+
+Event Handling: 
+    addEventListener, handling clicks, form submissions.
+
+Basic Debugging: 
+    using browser dev tools, debugger.
+
+    останалото е в линка
+
+
+
+*/
