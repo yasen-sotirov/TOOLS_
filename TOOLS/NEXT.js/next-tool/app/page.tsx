3efcdd067056f3211f030a1572d677a9c45534e4
Yasen Sotirov <ia.sotirov@gmail.com>
2024-07-02 10:37:49 +0300

    reordering

--- TOOLS/NEXT.js/next-tool/app/page.tsx
+++
@@ -1,7 +0,0 @@
-/*  ==App
-Home страницата на проекта 
-https://www.youtube.com/watch?v=GDd2c70gsxE&ab_channel=CodeCommerce  */
-
-export default function Home() {
-  return <h1>Welcome home !</h1>;
-}
