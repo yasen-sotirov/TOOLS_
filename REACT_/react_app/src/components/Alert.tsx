724277ed9c8584deea7ff04fe2d264179d9f83be
Yasen Sotirov <ia.sotirov@gmail.com>
2024-07-29 18:33:33 +0300

    reorder react tutoril

--- REACT_/react_app/src/components/Alert.tsx
+++
@@ -1,9 +0,0 @@
-// react arrow function export component
-// import React from "react";
-
-//https://youtu.be/SqcY0GlETPk?t=3703
-const Alert = () => {
-  return <div>Alert</div>;
-};
-
-export default Alert;
